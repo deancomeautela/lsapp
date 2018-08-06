3f2f8206320685c3a88245cb0521bd8e54f5956f
deancomeautela <deancomeautela@gmail.com>
2018-08-06 08:58:03 -0400

    Episode 9, user login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,26 +1,32 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href ="{{asset('css/app.css')}}">
-        <title>{{config('app.name', 'LSAPP')}}</title> 
-    </head>
-    
-    <body>
-            @include('inc.navbar')
-           <!--  <h1> { {$title}} <h1> This allows variables to be passsed from the controller into views dynamically... wow 
-            // also broke dynamic /posts route bc it didn't have a title variable-->
-          
-           
-            <div class = "container">
-            @include('inc.messages')
-            @yield('content')
-            </div>
-            <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
-            <script>
-                CKEDITOR.replace( 'article-ckeditor' );
-            </script>
-    </body>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+        @include('inc.navbar')
+        @include('inc.messages')
+        <div class='container'>
+                @yield('content')
+        </div>
+    </div>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
+    <script>
+        CKEDITOR.replace( 'article-ckeditor' );
+    </script>
+</body>
 </html>
